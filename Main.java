d0dd8b7d7deac8f1fc94122f540a2339c66f8029
Egemen <egemencagsirli@gmail.com>
2020-09-23 20:05:41 +0300

    second commit

--- Main.java
+++ Main.java
@@ -56,6 +56,15 @@ public class Main {
         System.out.println();
 
 
+        //stream - map, average, ifpresent
+        Arrays.stream(new int[] {2, 4, 6, 8, 10})
+                .map(x -> x * x)
+                .average()
+                .ifPresent(System.out::println);
+        System.out.println();
+
+
+
 
     }
 }
